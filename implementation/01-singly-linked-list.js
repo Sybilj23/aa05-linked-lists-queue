4c3dffecf1b5a58b1f45345dea3dfff371c06f33
atxmikaela <mikaela@mikaela@mikaelataylor.com>
2024-04-02 23:49:01 -0500

    finished singly linked

--- implementation/01-singly-linked-list.js
+++ implementation/01-singly-linked-list.js
@@ -44,37 +44,59 @@ class SinglyLinkedList {
     } else {
       let removedHead = this.head;
       if (this.head) this.head = this.head.next;
+      this.length--
       return removedHead;
 
     }
-    this.length--
+
   }
 
   removeFromTail() {
     if (!this.head) {
-        return undefined;
-      } else {
-        let removedHead = this.tail;
-        if (this.head) this.head = this.head.next;
-        return removedHead;
+      return undefined;
+    } else if (this.length === 1){
+      let removedTail = this.head;
+      this.head = null
+      this.length = 0;
+      return removedTail;
 
+
+    }else {
+      let removedTail;
+      let curr = this.head;
+      while (curr.next.next) {
+        curr = curr.next;
       }
-
+      removedTail = curr.next;
+      curr.next = null;
+      this.length--;
+      return removedTail;
+    }
   }
 
+
   peekAtHead() {
+    if (!this.head) {
+      return undefined;
+    } else {
+      return this.head.value;
+    }
     // Return the value of head node
     // Your code here
     // Write your hypothesis on the time complexity of this method here
   }
 
   print() {
-    // Print out the linked list
-    // Your code here
-    // Write your hypothesis on the time complexity of this method here
+    let curr = this.head;
+
+    while (curr) {
+      console.log(`${curr.value}`);
+      curr = curr.next;
+    }
+
+
   }
 }
-
 module.exports = {
   SinglyLinkedList,
   SinglyLinkedNode,
